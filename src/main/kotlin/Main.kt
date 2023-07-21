21db95e1e48623f97a3fae56231e424b047ca6ff
Erick <eryque7@gmail.com>
2023-07-21 19:21:36 +0300

    second comit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+fun main () {
+    print("Hello world")
+    // declaring variables
+    // val - value, var - varying
+    val number1 = 2
+    var number2 = 5
+    val a = number1 + number2
+    print("The answer is $a")//dollarsign mean the address(go to the address of a and pick the value
+    // Data type defines the type and kind of data being stored
+    // integers(int,long) Decimal points(Floats(Float), Doubles(Double)),
+    // text (characters(char), strings (string)), Logic (Boolean), Bytes
+    val num1:int =12 // interger
+    val num2:long = 12
+    val num3 = num1 +num2
+
+    val flo4: Float = 0.3f // floats should end with f
+    val do4: Double = 0.3
+    print (flo4)
+
+    val c: char = "A" // 1 byt character
+    val s: String = " This is a string in Kotlin" // stores 2 bytes
+    val x:Boolean = true
+
+    // Assignment -
+    //1. write a program that will calculate the difference
+    // between the year you were born and current year
+    //2.  c2 = a2 + b2. If Right angled triangle has base 4cm, height 3cm
+    // Find the hypotenuse of the triangle.
+}
+
